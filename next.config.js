71b0dc5b82d4f3ee7704b494cb43dc588b3d623a
RafalPloszka <rafalploszka@live.com>
2022-12-23 14:14:51 +0100

    next config setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: { subsets: ["latin", "latin-ext"] },
+      },
+    ],
+  },
+  i18n: {
+    locales: ["pl"],
+    defaultLocale: "pl",
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
