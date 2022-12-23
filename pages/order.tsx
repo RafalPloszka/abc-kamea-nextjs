9161711f420e92473ef9a86c1948de1d38d97b34
RafalPloszka <rafalploszka@live.com>
2022-12-23 14:34:02 +0100

    compose order page

---
+++ pages/order.tsx
@@ -0,0 +1,38 @@
+import { OrderForm } from "@/components/OrderForm";
+import { OrderTutorial } from "@/components/OrderTutorial";
+import Head from "next/head";
+
+export default function Order() {
+  return (
+    <>
+      <Head>
+        <title>ABC Kamea - Pieczątki Gorzów Wlkp.</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <OrderTutorial />
+        <OrderForm />
+      </div>
+    </>
+  );
+}
