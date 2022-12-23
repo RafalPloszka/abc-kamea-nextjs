b83da02db8fa5ef220a87ccd100e09635d393179
RafalPloszka <rafalploszka@live.com>
2022-12-23 14:18:36 +0100

    typescript app setup

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import { Kanit } from "@next/font/google";
+
+const kanit = Kanit({
+  weight: ["300", "400", "500", "600"],
+  subsets: ["latin", "latin-ext"],
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <main className={kanit.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
+
+export default MyApp;
