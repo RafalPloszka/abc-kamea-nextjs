b21ef4fdba92e30af70a84bfa7a4a551eabaaa21
RafalPloszka <rafalploszka@live.com>
2023-01-30 15:52:11 +0100

    update engraver photos

--- components/EngraverSection/EngraverCarousel.tsx
+++ components/EngraverSection/EngraverCarousel.tsx
@@ -1,15 +1,17 @@
 import { Carousel, CarouselImage } from "components/Carousel";
 
 import engraverPic1 from "../../public/engraver/plate.jpeg";
-import engraverPic2 from "../../public/engraver/machine.jpeg";
+import engraverPic2 from "../../public/engraver/phone.jpeg";
 import engraverPic3 from "../../public/engraver/seal_ring.jpeg";
 import engraverPic4 from "../../public/engraver/plate_silver.jpg";
+import engraverPic5 from "../../public/engraver/engraver_machine.jpeg";
 
 const engraverImages: CarouselImage[] = [
   { src: engraverPic1, alt: "Grawerowana tabliczka na drzwi" },
-  { src: engraverPic2, alt: "Maszyna do grawerowania" },
+  { src: engraverPic2, alt: "Wygrawerowany numer telefonu" },
   { src: engraverPic3, alt: "Referentki" },
   { src: engraverPic4, alt: "Grawerowana srebrna tabliczka" },
+  { src: engraverPic5, alt: "Maszyna grawerująca" },
 ];
 
 export const EngraverCarousel = () => (
