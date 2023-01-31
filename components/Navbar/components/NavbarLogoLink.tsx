3be9bcc4c24ff783f5a026987b339ab2e36c7df8
RafalPloszka <rafalploszka@live.com>
2023-01-31 14:18:10 +0100

    increase logo size

--- components/Navbar/components/NavbarLogoLink.tsx
+++ components/Navbar/components/NavbarLogoLink.tsx
@@ -10,8 +10,8 @@ export const NavbarLogoLink = () => {
       title="Company"
       className="inline-flex items-center"
     >
-      <Image src={logo} alt="logo" width={50} height={50} />
-      <span className="ml-2 text-xl font-semibold tracking-wide text-gray-800 uppercase">
+      <Image src={logo} alt="logo" width={80} height={80} />
+      <span className="ml-4 text-xl font-medium tracking-wide text-gray-800 uppercase">
         ABC Kamea
       </span>
     </Link>
